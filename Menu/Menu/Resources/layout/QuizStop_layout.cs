f983c405c8467f640c98e6c327e6a24874e9be93
Naveen080 <37764648+Naveen080@users.noreply.github.com>
2020-09-05 12:36:49 +0530

    Create QuizStop_layout.cs

---
+++ Menu/Menu/Resources/layout/QuizStop_layout.cs
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
+    android:orientation="vertical"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent">
+    <TextView
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        android:textSize="35dp"
+        android:text="congratulations"
+        android:layout_marginTop="100dp"
+        android:textAlignment="center"
+        android:textColor="#216561"/>
+    <TextView
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        android:id="@+id/score"
+        android:textAlignment="center"
+        android:layout_marginTop="20dp"/>
+    <Button
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:id="@+id/button1"
+        android:layout_marginTop="20dp"
+        android:layout_marginLeft="150dp"
+        android:text="Exit"/>
+</LinearLayout>
