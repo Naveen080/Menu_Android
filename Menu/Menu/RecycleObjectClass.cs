f67442525157ecbb6da5f1fa3bf251790b55cdcb
Naveen080 <37764648+Naveen080@users.noreply.github.com>
2020-09-17 20:45:38 +0530

    Create RecycleObjectClass.cs

---
+++ Menu/Menu/RecycleObjectClass.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Menu
+{
+    public class RecycleObjectClass
+    {
+        public int id
+        {
+            get;set;
+        }
+        public string TicketID
+        {
+            get;
+            set;
+        }
+        public string TicketDetails
+        {
+            get;
+            set;
+        }
+        
+    }
+}
